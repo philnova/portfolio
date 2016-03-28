869719ab5593781fcd64e2ab3ddceb736d62aeb4
Phil Nova <pnova8@gmail.com>
2016-03-28 15:58:26 -0700

    added ForgetMeNot

--- project_fileparser.py
+++ project_fileparser.py
@@ -12,7 +12,7 @@ def parse_project_file(filename):
 				title, description, link, github, image_url, technologies, nickname = line_array[0], line_array[1], line_array[2], line_array[3], line_array[4], line_array[5], line_array[6]
 				current_project = project.Project(title, description, link, github, image_url, technologies, nickname)
 				project_array.append(current_project)
-	random.shuffle(project_array)
+	#random.shuffle(project_array)
 	return project_array
 
 portfolio.open_projects_page(parse_project_file("projects_db.txt"))
